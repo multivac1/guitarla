89dd7dda2fb7f0eeae442e45e95e66a20d9084b4
multivac1 <nicoceluempresa@gmail.com>
2024-04-19 00:04:37 -0300

    feat: Create data base and map guitars data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Guitar from './components/Guitar';
+import { db } from './data/db';
+import { GuitarProps } from './types/GuitarProps';
 
 function App() {
+    const [data, setData] = useState<GuitarProps[]>(db);
     return (
         <>
             <Header />
@@ -10,7 +14,9 @@ function App() {
                 <h2 className="text-center">Nuestra Colección</h2>
 
                 <div className="row mt-5">
-                    <Guitar />
+                    {data.map((guitarData) => (
+                        <Guitar key={guitarData.id} guitarData={guitarData} />
+                    ))}
                 </div>
             </main>
 
---
+++ src/types/GuitarProps.ts
@@ -0,0 +1,7 @@
+export type GuitarProps = {
+    id: number;
+    name: string;
+    image: string;
+    description: string;
+    price: number;
+};
